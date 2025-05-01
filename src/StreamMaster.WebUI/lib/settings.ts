4dc86e3f069de5a7909e000f6b9d116a4f6101a7
FB <fbradyirl@users.noreply.github.com>
2025-05-01 22:56:03 +0100

    Update settings.ts for HA Ingress base URL

--- src/StreamMaster.WebUI/lib/settings.ts
+++ src/StreamMaster.WebUI/lib/settings.ts
@@ -4,9 +4,11 @@ export const isClient = typeof window !== "undefined";
 
 export const isDev = process.env.NODE_ENV === "development";
 
+const basePath = window.location.pathname.split('/').slice(0, -1).join('/');
+
 const loadConfig = () => {
 	const request = new XMLHttpRequest();
-	request.open("GET", "/config.json", false); // Synchronous request
+	request.open("GET", `${basePath}/config.json`, false); // use dynamic base path. Synchronous request
 	request.send(null);
 
 	if (request.status === 200) {
